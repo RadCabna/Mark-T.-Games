98a9e3d632c63b2c159a22a1ff01ddb09ce10620
Radcabna <radcabna@gmail.com>
2025-07-02 16:26:06 +0300

    Initial Commit

---
+++ Mark T. Games/Mark_T__GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mark_T__GamesApp.swift
+//  Mark T. Games
+//
+//  Created by Алкександр Степанов on 02.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Mark_T__GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
